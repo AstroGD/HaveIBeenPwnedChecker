225a3f057e0062bb8b330aac0a364f73d9925698
Lukas | AstroGD <lukas@astrogd.eu>
2022-06-13 16:13:40 +0200

    Update installation script
    :hammer: Builds will now be bundled with an app.asar file to improve performance and installation time

--- build/win64.js
+++ build/win64.js
@@ -6,7 +6,7 @@ async function bundleElectronApp(options) {
     const appPaths = await packager(options)
     console.log(`Electron app bundles created:\n${appPaths.join("\n")}`);
     fs.renameSync(path.join(__dirname, "../bin/HaveIBeenPwned Checker-win32-x64/LICENSE"), path.join(__dirname, "../bin/HaveIBeenPwned Checker-win32-x64/LICENSE.github"));
-    fs.copyFileSync(path.join(__dirname, "../bin/LICENSE"), path.join(__dirname, "../bin/HaveIBeenPwned Checker-win32-x64/LICENSE"));
+    fs.copyFileSync(path.join(__dirname, "../License.md"), path.join(__dirname, "../bin/HaveIBeenPwned Checker-win32-x64/LICENSE"));
     console.log(`LICENSE files updated`);
 }
 
@@ -14,6 +14,7 @@ const options = {
     appCopyright: `(c) 2020-${new Date().getFullYear()} AstroGD Lukas Weber`,
     arch: "x64",
     dir: path.join(__dirname, "../"),
+    asar: true,
     executableName: "HaveIBeenPwnedChecker",
     icon: path.join(__dirname, "/icon.ico"),
     ignore: /^.*\b(docs|coverage|build|jsdoc\.json|\.gitignore|README\.md)\b.*$/mi,
